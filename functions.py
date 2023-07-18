6166c975282256c64bb13f23cd525ecba7ae48a6
Mitama-san <106978598+Mitama-san@users.noreply.github.com>
2023-07-18 22:55:21 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from tensorflow.keras.layers import TextVectorization
+from langdetect import detect
 
-df = pd.read_csv("book_data.csv")
-df.dropna(subset=["genres", "book_desc"], inplace=True)
+df = pd.read_csv("cleaned_data_checkpoint.csv")
 
-def clean_text(text):
+def standardize_text(text):
     text = text.lower()
     text = re.sub(r'[^\w\s]', ' ', text)
     text = re.sub(r'\s+', ' ', text).strip()
@@ -18,26 +18,26 @@ def clean_text(text):
 max_length = 200  # Define the maximum length of sequences
 
 vectorizer = TextVectorization(
-    max_tokens=10000,
+    max_tokens=20000,
     output_sequence_length=max_length,
     output_mode="int"
     )
 
-text_data = df['book_desc'].values  # Assuming 'summary' column contains the book summaries
+text_data = df['desc'].values  
 vectorizer.adapt(text_data)
 
-def test_vectorizer(text):
-    a = clean_text(text)
+def reformat(text):
+    a = standardize_text(text)
     a = vectorizer(a)
     a = np.reshape(a, (1, max_length))
     return a
 
-def reviewBook(model,text):
-    labels = ['fiction', 'nonfiction']
-    a = clean_text(text)
-    a = vectorizer(a)
+def predict_genre(model,text):
+    a = reformat(text)
     a = np.reshape(a, (1, max_length))
     output = model.predict(a, batch_size=1)
-    score = (output>0.5)*1
-    pred = score.item()
-    return labels[pred]
+    if output < 0.5:
+      return "fiction"
+    else:
+      return "non-fiction"
+
